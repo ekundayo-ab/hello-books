791e717f26c8c03444284e0aeca8ff38e54d8b36
Ekundayo Abiona <ekundayoabiona@gmail.com>
2017-08-03 20:39:48 +0100

    more routes to see all books, update books, create books and borrow book. logic still in progress.

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,4 +1,7 @@
 const usersController = require('../controllers').users;
+const booksController = require('../controllers').books;
+const borrowsController = require('../controllers').borrows;
+const usersMiddleware = require('../middlewares').users;
 
 module.exports = (app) => {
   app.get('/api', (req, res) => res.status(200).send({
@@ -7,4 +10,10 @@ module.exports = (app) => {
 
   app.post('/api/users/signup', usersController.create);
   app.post('/api/users/signin', usersController.retrieve);
+  app.use(usersMiddleware.authenticate);
+  app.get('/api/users', usersController.list);
+  app.post('/api/books', booksController.create);
+  app.put('/api/books/:bookId', booksController.update);
+  app.get('/api/books', booksController.list);
+  app.post('/api/users/:userId/books', borrowsController.create);
 };
