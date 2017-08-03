73e28d5841a81dbcf3eac550a87d77f9cb262a0c
Ekundayo Abiona <ekundayoabiona@gmail.com>
2017-08-03 10:39:08 +0100

    Make the schema available to store borrowed books and returned books records in the database.

---
+++ server/migrations/20170802145450-create-borrow.js
@@ -0,0 +1,50 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    queryInterface.createTable('Borrows', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Users',
+          key: 'id',
+          as: 'userId',
+        },
+      },
+      bookId: {
+        type: Sequelize.INTEGER,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Books',
+          key: 'id',
+          as: 'bookId',
+        },
+      },
+      quantity: {
+        type: Sequelize.INTEGER,
+        defaultValue: 1,
+        allowNull: false,
+      },
+      returned: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: (queryInterface /* , Sequelize */) => {
+    queryInterface.dropTable('Borrows');
+  },
+};
