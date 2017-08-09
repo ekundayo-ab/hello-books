98bbb09a9d62e23c178fd5bb824a8882fb6f3365
Ekundayo Abiona <ekundayoabiona@gmail.com>
2017-08-09 16:47:32 +0100

    test default/root directory of the application.

--- test/bookTest.js
+++ test/bookTest.js
@@ -1,37 +1,16 @@
-import chai, { expect } from 'chai';
-import chaiHttp from 'chai-http';
-import app from '../app';
-import User from '../server/models/book';
-import seed from '../server/seeders/user';
+import chai from 'chai';
+import supertest from 'supertest';
 
 process.env.NODE_ENV = 'test';
 
-chai.use(chaiHttp);
+const should = chai.should();
+const expect = chai.expect();
+const api = supertest('http://localhost:8000');
 
-// const api = app();
-
-describe('API Routes', () => {
-  // start with a fresh DB 
-  beforeEach((done) => {
-    User.sequelize.sync({ force: true, match: /_test$/, logging: false })
-      .then(() => {
-        return seed(User);
-      }).then(() => {
-        done();
-      });
+describe('API', () => {
+  it('should return a 404 response with instructions', (done) => {
+    api.get('/')
+      .set('Accept', 'application/json')
+      .expect(404, done);
   });
 });
-
-describe('GET /v1/users', (done) => {
-  it('should get a list of users', (done) => {
-    chai.request(app)
-      .get('/v1/users')
-      .end((err, res) => {
-        expect(res.status).to.equal(200);
-        expect(res).to.be.json;
-        expect(res.body).to.be.a('array');
-        done();
-      });
-  });
-});
-
