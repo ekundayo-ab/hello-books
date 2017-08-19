5769afdec1d079db89f9ae80bf0ce00af02af04a
Ekundayo Abiona <ekundayoabiona@gmail.com>
2017-08-19 05:38:24 +0100

    Change swaggerDefinition to detect heroku port

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const swaggerDefinition = {
     version: '1.0.0',
     description: 'An application that helps manage a library and its processes like stocking, tracking and renting of books.',
   },
-  host: 'localhost:8000',
+  host: `localhost:${port}`,
   basePath: '/api/v1',
 };
 
