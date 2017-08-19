5a51ff77343153d75acf627ff97ea2694a66fd78
Ekundayo Abiona <ekundayoabiona@gmail.com>
2017-08-19 08:35:06 +0100

    Point API documentation page to live heroku domain

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const swaggerDefinition = {
     version: '1.0.0',
     description: 'An application that helps manage a library and its processes like stocking, tracking and renting of books.',
   },
-  host: `localhost:${port}`,
+  host: 'https://hellobooks-ekundayo.herokuapp.com',
   basePath: '/api/v1',
 };
 
