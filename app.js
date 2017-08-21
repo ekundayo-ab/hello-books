6015e18084889029da1bd526622ca877e3678743
Ekundayo Abiona <ekundayoabiona@gmail.com>
2017-08-21 18:40:19 +0100

    Still debugging API documentation CORS

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ const port = process.env.PORT || 8000;
 const app = express();
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE, HEAD');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-PINGOTHER, Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 const swaggerJSDoc = swagger;
