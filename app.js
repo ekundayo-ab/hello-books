93137149f8949b852153bf9108841ada81a7ace9
Ekundayo Abiona <ekundayoabiona@gmail.com>
2017-08-21 18:17:35 +0100

    Allow Cross Origin Requests to be served

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ require('dotenv').config();
 
 const port = process.env.PORT || 8000;
 const app = express();
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 const swaggerJSDoc = swagger;
 // swagger definition
 const swaggerDefinition = {
