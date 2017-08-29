edeaa0b1418471fa12d2b8b0cfe95f23f8a02c5a
Ekundayo Abiona <ekundayoabiona@gmail.com>
2017-08-29 15:21:45 +0100

    Adds test cases for listing of books borrowed but not returned

--- tests/testApi.js
+++ tests/testApi.js
@@ -13,6 +13,7 @@ const server = supertest.agent(app);
 let adminToken; // Token for an Admin User
 let normalToken; // Token for a Normal User
 let userId;
+let user2Id;
 let bookId;
 describe('API Operations', () => {
   before((done) => {
@@ -535,6 +536,53 @@ describe('API Operations', () => {
           expect(res.body.success).to.equal(false);
           expect(res.body.message).to.equal('Book borrowed already');
           expect(res.status).to.equal(409);
+          done();
+        });
+    });
+  });
+
+  describe('Upon listing of books not returned', () => {
+    before((done) => {
+      jwt.verify(adminToken, 'hello-books', (error, decoded) => {
+        userId = decoded.data.id;
+      });
+      jwt.verify(normalToken, 'hello-books', (error, decoded) => {
+        user2Id = decoded.data.id;
+      });
+      done();
+    });
+    it('should disallow unauthenticated user from listing', (done) => {
+      server
+        .get(`/api/v1/users/${userId}/books?returned=false`)
+        .set('Accept', 'application/x-www-form-urlencoded')
+        .end((err, res) => {
+          expect(res.body.success).to.equal(false);
+          expect(res.status).to.equal(401);
+          expect(res.body.message).to.equal('Unauthenticated, token not found');
+          done();
+        });
+    });
+    it('should list all books borrowed but not returned by authenticated user', (done) => {
+      server
+        .get(`/api/v1/users/${userId}/books?returned=false`)
+        .set('Accept', 'application/x-www-form-urlencoded')
+        .set('x-access-token', adminToken)
+        .end((err, res) => {
+          expect(res.body.success).to.equal(true);
+          expect(res.status).to.equal(200);
+          expect(res.body).to.have.property('borrow');
+          done();
+        });
+    });
+    it('should notify if user has no book(s) to return', (done) => {
+      server
+        .get(`/api/v1/users/${user2Id}/books?returned=false`)
+        .set('Accept', 'application/x-www-form-urlencoded')
+        .set('x-access-token', normalToken)
+        .end((err, res) => {
+          expect(res.body.success).to.equal(false);
+          expect(res.status).to.equal(404);
+          expect(res.body.message).to.equal('You have no books to return');
           done();
         });
     });
