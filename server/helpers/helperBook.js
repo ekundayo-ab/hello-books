dc8024e3baa80cd45da3afa390fa2e91058fb416
Ekundayo Abiona <ekundayoabiona@gmail.com>
2017-08-31 02:54:59 +0100

    Refactors helpers for books and users into proper objects

--- server/helpers/helperBook.js
+++ server/helpers/helperBook.js
@@ -1,72 +1,114 @@
-const book1 = {
-  body: {
-    isbn: '001',
+export default {
+  book1: {
+    body: {
+      isbn: '001',
+      title: 'Learn Haskell',
+      author: 'Haskell Master',
+      description: 'Learn and Master Haskell in 16 Months',
+      quantity: 20,
+    },
+  },
+  book2: {
+    body: {
+      isbn: '001',
+      title: 'Learn Haskell',
+      author: 'Haskell Master',
+      description: 'Learn and Master Haskell in 16 Months',
+    },
+  },
+  book3: {
+    body: {
+      isbn: '',
+      title: 'Learn Haskell',
+      author: 'Haskell Master',
+      description: 'Learn and Master Haskell in 16 Months',
+      quantity: 100,
+    },
+  },
+  book4: {
+    body: {
+      isbn: '001',
+      title: '',
+      author: 'Haskell Master',
+      description: 'Learn and Master Haskell in 16 Months',
+      quantity: 120,
+    },
+  },
+  book5: {
+    body: {
+      isbn: '001',
+      title: 'Learn Haskell',
+      author: '',
+      description: 'Learn and Master Haskell in 16 Months',
+      quantity: 300,
+    },
+  },
+  book6: {
+    body: {
+      isbn: '001',
+      title: 'Learn Haskell',
+      author: 'Haskell Master',
+      description: '',
+      quantity: 200,
+    },
+  },
+  book7: {
+    body: {
+      isbn: '001',
+      title: 'Learn Haskell',
+      author: 'Haskell Master',
+      description: 'Learn and Master Haskell in 16 Months',
+      quantity: '',
+    },
+  },
+  book8: {
+    isbn: 1,
     title: 'Learn Haskell',
     author: 'Haskell Master',
     description: 'Learn and Master Haskell in 16 Months',
     quantity: 20,
   },
-};
-const book2 = {
-  body: {
-    isbn: '001',
+  book9: {
+    isbn: 'number',
     title: 'Learn Haskell',
     author: 'Haskell Master',
     description: 'Learn and Master Haskell in 16 Months',
+    quantity: 20,
   },
-};
-const book3 = {
-  body: {
-    isbn: '',
+  book10: {
+    isbn: 21,
     title: 'Learn Haskell',
     author: 'Haskell Master',
     description: 'Learn and Master Haskell in 16 Months',
-    quantity: 100,
+    quantity: 'number',
   },
-};
-const book4 = {
-  body: {
-    isbn: '001',
-    title: '',
-    author: 'Haskell Master',
-    description: 'Learn and Master Haskell in 16 Months',
-    quantity: 120,
+  book11: {
+    isbn: 45,
+    title: 'Learn JAVA',
+    author: 'JAVAMan',
+    description: 'Learn JAVA in 8 weeks',
+    quantity: 20,
   },
-};
-const book5 = {
-  body: {
-    isbn: '001',
-    title: 'Learn Haskell',
-    author: '',
-    description: 'Learn and Master Haskell in 16 Months',
-    quantity: 300,
+  book12: {
+    isbn: null,
+    title: null,
+    author: null,
+    description: null,
+    quantity: null,
   },
-};
-const book6 = {
-  body: {
-    isbn: '001',
-    title: 'Learn Haskell',
-    author: 'Haskell Master',
-    description: '',
-    quantity: 200,
+  book13: {
+    isbn: undefined,
+    title: undefined,
+    author: undefined,
+    description: undefined,
+    quantity: undefined,
   },
-};
-const book7 = {
-  body: {
-    isbn: '001',
-    title: 'Learn Haskell',
-    author: 'Haskell Master',
-    description: 'Learn and Master Haskell in 16 Months',
-    quantity: '',
+  book14: {
+    isbn: 23,
+    title: 'Learn Android',
+    author: 'AndroidMan',
+    description: 'Learn Android in 8 weeks',
+    quantity: 20,
   },
 };
 
-export default {
-  book1,
-  book2,
-  book3,
-  book4,
-  book5,
-  book6,
-  book7,
-};
--- server/helpers/helperUser.js
+++ server/helpers/helperUser.js
@@ -1,73 +1,76 @@
-const user1 = {
-  decoded: {
-    data: {
-      id: 10,
-      username: 'ekundayo',
-      password: '$2a$10$bhVz30gNLdCmqbZYuRIPQulAB/SgDA5XwS9CIgKfSTYod1ezymF2.',
-      email: 'ekprogs@gmail.com',
-      role: 'admin',
-      level: 'bronze',
-    },
-    iat: 1504038990,
-    exp: 1504042590,
-  },
-};
-const user2 = {
-  decoded: {
-    data: {
-      id: 10,
-      username: 'ekundayo',
-      password: '$2a$10$bhVz30gNLdCmqbZYuRIPQulAB/SgDA5XwS9CIgKfSTYod1ezymF2.',
-      email: 'ekprogs@gmail.com',
-      role: 'normal',
-      level: 'bronze',
-    },
-    iat: 1504038990,
-    exp: 1504042590,
-  },
-};
-const user3 = {
-  username: 'ekundayo',
-  password: '$2a$10$bhVz30gNLdCmqbZYuRIPQulAB/SgDA5XwS9CIgKfSTYod1ezymF2.',
-  email: 'ekprogs@gmail.com',
-};
-const user4 = {
-  username: 'ekundayo',
-  password: '$2a$10$bhVz30gNLdCmqbZYuRIPQulAB/SgDA5XwS9CIgKfSTYod1ezymF2.',
-  email: 'ekprogs',
-};
-const user5 = {
-  username: 'ekundayo',
-  password: '$2a$10$bhVz30gNLdCmqbZYuRIPQulAB/SgDA5XwS9CIgKfSTYod1ezymF2.',
-  email: 'ekprogs@gmail',
-};
-const user6 = {
-  username: 'emma',
-  password: '$2a$10$bhVz30gNLdCmqbZYuRIPQulAB/SgDA5XwS9CIgKfSTYod1ezymF2.',
-  email: 'emma@gmail.com',
-};
-const user7 = {
-  username: 'kate',
-  password: '$2a$10$bhVz30gNLdCmqbZYuRIPQulAB/SgDA5XwS9CIgKfSTYod1ezymF2.',
-  email: 'emma@gmail.com',
-};
-const user8 = {
-  username: '',
-  password: 'calmsecret',
-  email: 'calm@gmail.com',
-};
-const user8a = {
-  username: 'good',
-  password: '',
-  email: 'good@gmail.com',
-};
-const user8b = {
-  username: 'gentle',
-  password: 'gentlesecret',
-  email: '',
-};
-
-
 export default {
-  user1, user2, user3, user4, user5, user6, user7, user8, user8a, user8b,
+  user1: {
+    decoded: {
+      data: {
+        id: 10,
+        username: 'ekundayo',
+        password: '$2a$10$bhVz30gNLdCmqbZYuRIPQulAB/SgDA5XwS9CIgKfSTYod1ezymF2.',
+        email: 'ekprogs@gmail.com',
+        role: 'admin',
+        level: 'bronze',
+      },
+      iat: 1504038990,
+      exp: 1504042590,
+    },
+  },
+  user2: {
+    decoded: {
+      data: {
+        id: 10,
+        username: 'ekundayo',
+        password: '$2a$10$bhVz30gNLdCmqbZYuRIPQulAB/SgDA5XwS9CIgKfSTYod1ezymF2.',
+        email: 'ekprogs@gmail.com',
+        role: 'normal',
+        level: 'bronze',
+      },
+      iat: 1504038990,
+      exp: 1504042590,
+    },
+  },
+  user3: {
+    username: 'ekundayo',
+    password: '$2a$10$bhVz30gNLdCmqbZYuRIPQulAB/SgDA5XwS9CIgKfSTYod1ezymF2.',
+    email: 'ekprogs@gmail.com',
+  },
+  user4: {
+    username: 'ekundayo',
+    password: '$2a$10$bhVz30gNLdCmqbZYuRIPQulAB/SgDA5XwS9CIgKfSTYod1ezymF2.',
+    email: 'ekprogs',
+  },
+  user5: {
+    username: 'ekundayo',
+    password: '$2a$10$bhVz30gNLdCmqbZYuRIPQulAB/SgDA5XwS9CIgKfSTYod1ezymF2.',
+    email: 'ekprogs@gmail',
+  },
+  user6: {
+    username: 'emma',
+    password: '$2a$10$bhVz30gNLdCmqbZYuRIPQulAB/SgDA5XwS9CIgKfSTYod1ezymF2.',
+    email: 'emma@gmail.com',
+  },
+  user7: {
+    username: 'kate',
+    password: '$2a$10$bhVz30gNLdCmqbZYuRIPQulAB/SgDA5XwS9CIgKfSTYod1ezymF2.',
+    email: 'emma@gmail.com',
+  },
+  user8: {
+    username: '',
+    password: 'calmsecret',
+    email: 'calm@gmail.com',
+  },
+  user8a: {
+    username: 'good',
+    password: '',
+    email: 'good@gmail.com',
+  },
+  user8b: {
+    username: 'gentle',
+    password: 'gentlesecret',
+    email: '',
+  },
+  user9: {
+    username: 'pate',
+    password: '$2a$10$bhVz30gNLdCmqbZYuRIPQulAB/SgDA5XwS9CIgKfSTYod1ezymF2.',
+    email: 'pate@gmail.com',
+  },
 };
+
