70b3ce4749da7dff1d55d7a5784244bc3000a219
Ekundayo Abiona <ekundayoabiona@gmail.com>
2017-09-08 18:08:33 +0100

    Fixes CSS lint errors and formats html

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/Landing/Home';
-// import { createStore, applyMiddleware } from 'redux';
-// import thunk from 'redux-thunk';
-// import rootReducer from './rootReducer';
-// import { composeWithDevTools } from 'redux-devtools-extension';
-// import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <Router>
