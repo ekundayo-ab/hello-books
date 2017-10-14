af27e85ce9171ca58809d94078de28bf7b0d4b52
Ekundayo Abiona <ekundayoabiona@gmail.com>
2017-10-14 16:18:49 +0100

    feature(set-authorization-level): configure to deploy on heroku

--- server/config/config.js
+++ server/config/config.js
@@ -16,6 +16,7 @@ module.exports = {
     dialect: 'postgres',
   },
   production: {
-    use_env_variable: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
   },
 };
