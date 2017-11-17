806a6fcdcd4e6535572bca333b20fd2a7e41e8c5
Ekundayo Abiona <ekundayoabiona@gmail.com>
2017-11-17 15:33:37 +0100

    chore(integrate-client-side-tests): ensure test(s) is/are written for all reducers

---
+++ client/tests/reducers/singleBorrowReducer.test.js
@@ -0,0 +1,20 @@
+import singleBorrowReducer from '../../src/reducers/singleBorrowReducer';
+import * as actionTypes from '../../src/actions/types';
+import {
+  borrow1,
+} from './testData';
+
+describe('single borrow reducer', () => {
+  it('should return the initial state', () => {
+    expect(singleBorrowReducer({}, {})).toEqual({});
+  });
+
+  it('should handle BOOK_FETCHED and save the payload to the store', () => {
+    expect(
+      singleBorrowReducer(
+        { borrow: {} },
+        { type: actionTypes.BORROWED_FETCHED, borrow: borrow1 }
+      )
+    ).toEqual({ borrow: borrow1 });
+  });
+});
